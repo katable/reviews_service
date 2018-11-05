f7f00f7006a8bc2772cbc0fc571adcf02388215c
Angela Zhou <angelazhou922@gmail.com>
2018-11-04 20:14:48 -0800

    pagination progress

---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import style from './style.css';
+
+const SearchBar = (props) => (
+  <div>
+    <form className={style.searchContainer}>
+      <input type="text" placeholder="&#xF002; Search" name="search" onChange={props.onSearch} />
+    </form>
+  </div> 
+); 
+
+export default SearchBar;
+
+
+// const SearchBar = (props) => (
+//   <div>
+//     <form className={style.searchContainer}>
+//       <input type="text" placeholder="Search.." name="search" onChange={props.onSearch}/>
+//       <button type="submit" className={style.searchButton}><i className="fa fa-search"></i></button>
+//     </form>
+//   </div> 
+// ); 
+
+// export default SearchBar;
