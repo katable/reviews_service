eb925cd34f9e02ac02a903e8fd3341e3feb26731
Angela Zhou <angelazhou922@gmail.com>
2018-11-04 20:07:52 -0800

    styling

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -4,10 +4,21 @@ import style from './style.css';
 const SearchBar = (props) => (
   <div>
     <form className={style.searchContainer}>
-      <input type="text" placeholder="Search.." name="search" onChange={props.onSearch}/>
-      <button type="submit" className={style.searchButton}><i className="fa fa-search"></i></button>
+      <input type="text" placeholder="&#xf167; Search" name="search" onChange={props.onSearch} />
     </form>
   </div> 
 ); 
 
 export default SearchBar;
+
+
+// const SearchBar = (props) => (
+//   <div>
+//     <form className={style.searchContainer}>
+//       <input type="text" placeholder="Search.." name="search" onChange={props.onSearch}/>
+//       <button type="submit" className={style.searchButton}><i className="fa fa-search"></i></button>
+//     </form>
+//   </div> 
+// ); 
+
+// export default SearchBar;
